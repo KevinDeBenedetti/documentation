aab8edf6ee3af252741568852f25c984d1909ccb
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-10-08 00:36:18 +0200

    feat: add ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,24 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    rules: {
+      'indent': ['error', 2, {
+        'SwitchCase': 1,
+        'ignoredNodes': ['TemplateLiteral']
+      }],
+      'vue/html-indent': ['error', 2],
+      'vue/script-indent': ['error', 2, {
+        'baseIndent': 0,
+        'switchCase': 1
+      }],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'comma-dangle': ['error', 'never'],
+      'arrow-parens': ['error', 'always'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'space-before-function-paren': ['error', 'always']
+    }
+  }
 )
