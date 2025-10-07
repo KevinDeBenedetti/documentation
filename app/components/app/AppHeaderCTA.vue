e055dc76629b07bc862a384747e5bcd4904becb6
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-10-07 21:08:35 +0200

    feat: add admin component & api starter

---
+++ app/components/app/AppHeaderCTA.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const config = useRuntimeConfig() 
+const dev = config.public.isAdmin
+</script>
+
+<template>
+    <div v-if="dev">
+        <UButton to="/admin" icon="i-lucide-rocket" size="md" color="neutral" variant="solid">Admin</UButton>
+    </div>
+</template>
---
+++ server/api/test.ts
@@ -0,0 +1,4 @@
+export default defineEventHandler(async (event) => {
+  // ... Do whatever you want here
+  return { message: 'Hello from the server side!' }
+})
