a8de8160d95ad5c90e5530b417829fba8cdd21ed
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-10-11 22:41:14 +0200

    fix: update Admin button to use dynamic language routing

--- app/components/app/AppHeaderCTA.vue
+++ app/components/app/AppHeaderCTA.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
-const config = useRuntimeConfig() 
+const config = useRuntimeConfig()
 const dev = config.public.isAdmin
+const route = useRoute()
+const { defaultLocale } = useI18n()
+const lang = computed(() => (route.query.lang as string) ?? defaultLocale)
 </script>
 
 <template>
   <div v-if="dev">
-    <UButton to="/admin" icon="i-lucide-rocket" size="md" color="neutral" variant="solid">Admin</UButton>
+    <UButton 
+      :to="`/${lang}/admin`" 
+      icon="i-lucide-rocket" 
+      size="md" 
+      color="neutral" 
+      variant="solid">
+      Admin
+    </UButton>
   </div>
 </template>
