65d2784a069eef93a9366260682960ead8d74dff
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-10-19 20:40:24 +0200

    fix: update favicon path in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
         { name: 'description', content: 'Documentation site' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
       ]
     }
   },
